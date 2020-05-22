a1c3eb8fa57e884914d4115a417b370a41a6264d
Oliver Hedges <oliver@databridgeuk.com>
2020-05-22 11:47:13 +0100

    Makes it better

--- app/upload/scraper.py
+++ app/upload/scraper.py
@@ -20,8 +20,11 @@ class DataScraper:
         num = [(name_from_ix_element(i), i.get('sign', '') + i.text) for i in ix_num]
         num_dict = {k: [] for k in wanted} # use dict.setdefualt() later instead
         for k, v in num:
-            if k in wanted:
-                num_dict[k].append(int(v.replace(',', '')))
+            parsed_v = v.split('.')[0].replace(',', '').replace(' ','').replace('\\n', '').replace('--','-')
+            try:
+                num_dict[k].append('' if v == '-' else int(parsed_v))
+            except KeyError:
+                continue
         line = []
         for k in wanted:
             vs = num_dict[k][-2:] # take at most last two from scraped values
--- app/upload/views.py
+++ app/upload/views.py
@@ -7,7 +7,14 @@ def uploadfiles(request):
     URL = False
     form = UrlForm(request.POST or None)
     if request.method == 'POST' and form.is_valid():
+        # variable from user text input
         URL = form.cleaned_data['URL']
+
+        # import and run function to import data into Accounts model
+        if URL.startswith('http://download.companieshouse.gov.uk/Accounts_'):
+            from upload.getaccounts import get_accounts
+            get_accounts(URL)
+
     context = {'form':form,
             'URL':URL,
             }
