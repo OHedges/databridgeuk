15a686880e6d4289a063b7317ec7b751344ac574
Oliver Hedges <oliver@databridgeuk.com>
2020-05-25 11:12:14 +0100

    added bulk create

--- app/upload/getbasicdata.py
+++ app/upload/getbasicdata.py
@@ -23,66 +23,71 @@ def get_date(date):
 def get_num(num):
     return None if num == '' else int(num)
 
-
+batch_size = 100
+objs = []
 with open('/usr/src/app/upload/BasicCompanyDataAsOneFile-2020-05-01.csv') as f:
     reader = csv.reader(f)
     next(reader)
     for row in reader:
-        c = Company()
-        c.companyname = row[0]
-        c.companynumber = row[1]
-        c.regaddress_careof = row[2] 
-        c.regaddress_pobox = row[3]
-        c.regaddress_addressline1 = row[4] 
-        c.regaddress_addressline2 = row[5] 
-        c.regaddress_posttown = row[6] 
-        c.regaddress_county = row[7] 
-        c.regaddress_country = row[8] 
-        c.regaddress_postcode = row[9] 
-        c.companycategory = row[10] 
-        c.companystatus = row[11] 
-        c.countryoforigin = row[12] 
-        c.dissolutiondate = get_date(row[13])
-        c.incorporationdate = get_date(row[14])
-        c.accounts_accountrefday = get_num(row[15])
-        c.accounts_accountrefmonth = get_num(row[16])
-        c.accounts_nextduedate = get_date(row[17])
-        c.accounts_lastmadeupdate = get_date(row[18])
-        c.accounts_accountcategory = row[19]
-        c.returns_nextduedate = get_date(row[20])
-        c.returns_lastmadeupdate = get_date(row[21])
-        c.mortgages_nummortcharges = row[22]
-        c.mortgages_nummortoutstanding = row[23]
-        c.mortgages_nummortpartsatisfied = row[24]
-        c.mortgages_nummortsatisfied = row[25]
-        c.siccode_sictext_1 = row[26] 
-        c.siccode_sictext_2 = row[27] 
-        c.siccode_sictext_3 = row[28] 
-        c.siccode_sictext_4 = row[29]
-        c.limitedpartnerships_numgenpartners = row[30]
-        c.limitedpartnerships_numlimpartners = row[31]
-        c.url = row[32]
-        c.previousname_1_condate = get_date(row[33])
-        c.previousname_1_companyname = row[34]
-        c.previousname_2_condate = get_date(row[35])
-        c.previousname_2_companyname = row[36]
-        c.previousname_3_condate = get_date(row[37])
-        c.previousname_3_companyname = row[38]
-        c.previousname_4_condate = get_date(row[39])
-        c.previousname_4_companyname = row[40]
-        c.previousname_5_condate = get_date(row[41])
-        c.previousname_5_companyname = row[42]
-        c.previousname_6_condate = get_date(row[43])
-        c.previousname_6_companyname = row[44]
-        c.previousname_7_condate = get_date(row[45])
-        c.previousname_7_companyname = row[46]
-        c.previousname_8_condate = get_date(row[47])
-        c.previousname_8_companyname = row[48]
-        c.previousname_9_condate = get_date(row[49])
-        c.previousname_9_companyname = row[50]
-        c.previousname_10_condate = get_date(row[51])
-        c.previousname_10_companyname = row[52]
-        c.confstmtnextduedate = get_date(row[53])
-        c.confstmtlastmadeupdate = get_date(row[54])
-        c.save()
+        objs.append(Company(
+        companyname = row[0],
+        companynumber = row[1],
+        regaddress_careof = row[2] ,
+        regaddress_pobox = row[3],
+        regaddress_addressline1 = row[4] ,
+        regaddress_addressline2 = row[5] ,
+        regaddress_posttown = row[6] ,
+        regaddress_county = row[7] ,
+        regaddress_country = row[8] ,
+        regaddress_postcode = row[9] ,
+        companycategory = row[10] ,
+        companystatus = row[11] ,
+        countryoforigin = row[12] ,
+        dissolutiondate = get_date(row[13]),
+        incorporationdate = get_date(row[14]),
+        accounts_accountrefday = get_num(row[15]),
+        accounts_accountrefmonth = get_num(row[16]),
+        accounts_nextduedate = get_date(row[17]),
+        accounts_lastmadeupdate = get_date(row[18]),
+        accounts_accountcategory = row[19],
+        returns_nextduedate = get_date(row[20]),
+        returns_lastmadeupdate = get_date(row[21]),
+        mortgages_nummortcharges = row[22],
+        mortgages_nummortoutstanding = row[23],
+        mortgages_nummortpartsatisfied = row[24],
+        mortgages_nummortsatisfied = row[25],
+        siccode_sictext_1 = row[26] ,
+        siccode_sictext_2 = row[27] ,
+        siccode_sictext_3 = row[28] ,
+        siccode_sictext_4 = row[29],
+        limitedpartnerships_numgenpartners = row[30],
+        limitedpartnerships_numlimpartners = row[31],
+        url = row[32],
+        previousname_1_condate = get_date(row[33]),
+        previousname_1_companyname = row[34],
+        previousname_2_condate = get_date(row[35]),
+        previousname_2_companyname = row[36],
+        previousname_3_condate = get_date(row[37]),
+        previousname_3_companyname = row[38],
+        previousname_4_condate = get_date(row[39]),
+        previousname_4_companyname = row[40],
+        previousname_5_condate = get_date(row[41]),
+        previousname_5_companyname = row[42],
+        previousname_6_condate = get_date(row[43]),
+        previousname_6_companyname = row[44],
+        previousname_7_condate = get_date(row[45]),
+        previousname_7_companyname = row[46],
+        previousname_8_condate = get_date(row[47]),
+        previousname_8_companyname = row[48],
+        previousname_9_condate = get_date(row[49]),
+        previousname_9_companyname = row[50],
+        previousname_10_condate = get_date(row[51]),
+        previousname_10_companyname = row[52],
+        confstmtnextduedate = get_date(row[53]),
+        confstmtlastmadeupdate = get_date(row[54]),
+        ))
+        if len(objs) > batch_size:
+            Company.objects.bulk_create(objs, ignore_conflicts=True)
+            objs = []
+    Company.objects.bulk_create(objs, ignore_conflicts=True)
 
